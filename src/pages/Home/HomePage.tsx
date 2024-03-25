e00445b101873b597dde7ff4f39dec486ba4c801
Alysson Sene <alyssonsene@outlook.com>
2024-03-25 13:24:03 -0300

    [FIX] Bug do scroll

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import FeedbackOrganism from '../../components/organisms/FeedbackOrganism'
 import FeedbackVideosOrganism from '../../components/organisms/FeedbackVideosOrganism'
 import HomePageOrganism from '../../components/organisms/HomePageOrganism'
@@ -9,26 +9,29 @@ import OurValuesOrganism from '../../components/organisms/OurValuesOrganism'
 import ProductsPageOrganism from '../../components/organisms/ProductsPageOrganism'
 
 const HomePage: React.FC = () => {
-	const links = document.querySelectorAll('.options-link')
-	const sections = document.querySelectorAll('section')
+	useEffect(() => {
+		const links = document.querySelectorAll('.options-link')
+		const sections = document.querySelectorAll('section')
 
-	window.addEventListener('scroll', () => {
-		sections.forEach(section => {
-			let top = window.scrollY
-			let offSet = section.offsetTop - 10
-			let heightSection = section.offsetHeight
-			let idSection = section.getAttribute('id')
+		window.addEventListener('scroll', () => {
+			sections.forEach(section => {
+				let top = window.scrollY
+				let offSet = section.offsetTop - 100
+				let heightSection = section.offsetHeight
+				let idSection = section.getAttribute('id')
 
-			if (top >= offSet && top <= offSet + heightSection) {
-				links.forEach(link => {
-					link.classList.remove('active')
-					document
-						.querySelector(`a[href*=${idSection}]`)
-						?.classList.add('active')
-				})
-			}
+				if (top >= offSet && top <= offSet + heightSection) {
+					links.forEach(link => {
+						link.classList.remove('active')
+						document
+							.querySelector(`a[href*=${idSection}]`)
+							?.classList.add('active')
+						console.log(offSet)
+					})
+				}
+			})
 		})
-	})
+	}, [])
 	return (
 		<>
 			<HomePageOrganism />
